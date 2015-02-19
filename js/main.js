d8ca24a3437f0134b383648fc0f5ba19947799ff
Jake Bapple <jake.bapple@gmail.com>
2015-02-19 17:05:35 -0600

    Many Changes to Index, Contac, Smith
    Lots of changes made

--- js/main.js
+++ js/main.js
@@ -20,11 +20,3 @@ $('a[href^="#"]').on('click', function(event) {
 	scrolltoDiv(url,1000,50)
 
 });
-
-$( document ).ready (function() {
-  if($(window).width() <= 415) {
-    $("img").each(function() {
-      $(this).attr("src", $(this).attr("src").replace("img/wsilogo.png", "img/wsilogo_small.fw.png"));
-    });
-  }
-});
