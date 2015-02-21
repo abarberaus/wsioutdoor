da412716b49fc3aa14d4867af930a9ddf2ce3e9c
abarberaus <abarberaus@gmail.com>
2015-02-21 17:44:50 -0600

    final change to php?

--- mailer.php
+++ mailer.php
@@ -31,7 +31,7 @@ $message
 ";
 
 /* Send the message using mail() function */
-mail($myemail, $subject, $message);
+mail($myemail, $subject, $message, $headers);﻿
 
 /* Redirect visitor to the thank you page */
 header('Location: http://www.makeemfeelweird.com');
