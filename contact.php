37de30ca9cc138f690bb55f4243eee89c9540f88
abarberaus <abarberaus@gmail.com>
2015-06-02 22:01:55 -0500

    deleted comments

--- contact.php
+++ contact.php
@@ -116,31 +116,6 @@
                                             </div>
                                         </div>
                                     </form>
-                                    <!-- test action -->
-                                    <!-- <form id="contact" method="post" action="/mailer.php" class="form" role="form">
-                                        <div class="row">
-
-                                        <div class="form-group">
-                                            <label for="inputName" class="col-lg-2 control-label">Name</label>
-                                            <div class="col-lg-10">
-                                                <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Your Name">
-                                            </div>                   
-
-                                            <div class="col-xs-12 col-md-6 form-group nopaddingleft">
-                                                <input class="form-control" id="name" name="name" placeholder="Name" type="text" required autofocus />
-                                            </div>
-                                            <div class="col-xs-12 col-md-6 form-group nopaddingright">
-                                                <input class="form-control" id="email" name="email" placeholder="Email" type="email" required />
-                                            </div>
-                                        </div>
-                                        <textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea>
-                                        <br />
-                                        <div class="row">
-                                            <div class="col-xs-12 col-md-12 form-group nopaddingright">
-                                                <button class="btn btn-primary pull-right" type="submit">Submit</button>
-                                            </div>
-                                        </div>
-                                    </form> -->
                                 </div>
                             </div>
                                     
