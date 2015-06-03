84cf1a936e0590682f578ca46b477e33a2e65157
bapplejax <jake.bapple@gmail.com>
2015-06-02 21:37:39 -0500

    Added My Email
    Trying to see if it's a firewall issue
    changes made 6/2/2015 @ 9:37

--- mailer.php
+++ mailer.php
@@ -1,12 +1,12 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "aaron@wsioutdoor.com"; // this is your Email address
+    $to = "aaron@wsioutdoor.com,jake.bapple@gmail.com"; // this is your Email address
     $from = $_POST['inputEmail']; // this is the sender's Email address
     $name = $_POST['inputName'];
     $subject = $_POST['inputSubject'];
     $my_message = $_POST['inputMessage'];
     $message = "Someone has sent you a message using your contact form: " + "Name: " + $name + " " + "Email: " + $from + " " + "Subject: " + $subject + " " + "Message: " + $my_message +".";
-    mail($to,$subject,$message);
+    mail($to, $subject, $message);
     header("Location: http://www.wsioutdoor.com");
 
     // You can also use header('Location: thank_you.php'); to redirect to another page.
