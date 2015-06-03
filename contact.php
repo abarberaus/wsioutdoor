bf23fb834c4a860204fed9e01eff8e9df837ab75
bapplejax <jake.bapple@gmail.com>
2015-06-02 20:50:43 -0500

    Contact form try
    changed the contact form to simpler

--- contact.php
+++ contact.php
@@ -85,38 +85,37 @@
                                 
                                 <div class="col-sm-8 contact-form">
                                     <form name="contactform" method="post" action="mailer.php" class="form-horizontal" role="form">
-                            <div class="form-group">
-                                <label for="inputName" class="col-lg-2 control-label">Name</label>
-                                <div class="col-lg-10">
-                                    <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Your Name">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputEmail1" class="col-lg-2 control-label">Email</label>
-                                <div class="col-lg-10">
-                                    <input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Your Email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputSubject" class="col-lg-2 control-label">Subject</label>
-                                <div class="col-lg-10">
-                                    <input type="text" class="form-control" id="inputSubject" name="inputSubject" placeholder="Subject Message">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputPassword1" class="col-lg-2 control-label">Message</label>
-                                <div class="col-lg-10">
-                                    <textarea class="form-control" rows="4" id="inputMessage" name="inputMessage" placeholder="Your message..."></textarea>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-lg-offset-2 col-lg-10">
-                                    <button type="submit" class="btn btn-default">
-                                        Send Message
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                                        <div class="form-group">
+                                            <label for="inputName" class="col-lg-2 control-label">Name</label>
+                                            <div class="col-lg-10">
+                                                <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Your Name" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                                            <div class="col-lg-10">
+                                                <input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Your Email" required>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputSubject" class="col-lg-2 control-label">Subject</label>
+                                            <div class="col-lg-10">
+                                                <input type="text" class="form-control" id="inputSubject" name="inputSubject" placeholder="Subject Message">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputPassword1" class="col-lg-2 control-label">Message</label>
+                                            <div class="col-lg-10">
+                                                <textarea class="form-control" rows="4" id="inputMessage" name="inputMessage" placeholder="Your message..."></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-lg-offset-2 col-lg-10">
+                                                <input type="submit" name="submit" class="btn btn-default" value="Send Message">
+                                                </input>
+                                            </div>
+                                        </div>
+                                    </form>
                                     <!-- test action -->
                                     <!-- <form id="contact" method="post" action="/mailer.php" class="form" role="form">
                                         <div class="row">
