b7d6f2a8525ff8ea394f63056cd218b3e87536a5
Jake Bapple <jake.bapple@gmail.com>
2015-09-14 18:20:03 -0500

    Created thank you page and rerouted to it

--- mailer.php
+++ mailer.php
@@ -7,7 +7,7 @@ if(isset($_POST['submit'])){
     $my_message = $_POST['inputMessage'];
     $message = "Someone has sent you a message using your contact form: " + "Name: " + $name + " " + "Email: " + $from + " " + "Subject: " + $subject + " " + "Message: " + $my_message +".";
     mail($to, $subject, $message);
-    header("Location: http://www.wsioutdoor.com");
+    header("Location: thank_you.php");
 
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
